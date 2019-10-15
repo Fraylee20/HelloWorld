ccff195876fa717812fc0bec70a200ff15ee5ba6
Berkay Çoktu <36849579+Fraylee20@users.noreply.github.com>
2019-10-15 22:09:00 +0300

    Update client.java

--- client.java
+++ client.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class client{
 
 public static void main(String[] args) throws IOException{
-	Socket s = new Socket("localhost",80);
+	Socket s = new Socket("169.254.195.169",80);
 	PrintWriter pr=new PrintWriter(s.getOutputStream());
 	pr.println("hello");
 	pr.flush();
